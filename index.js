8a08f2d384cb4f805b25d12bada44787a049d06d
Sinuhan <167394965+Hydraspamj@users.noreply.github.com>
2025-10-17 16:38:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 web: node index.js
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const app = express();
+const server = require('http').Server(app);
+
+const peerServer = ExpressPeerServer(server, {
+  path: '/peerjs'
+});
+
+app.use('/peerjs', peerServer);
