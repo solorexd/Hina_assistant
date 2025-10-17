9cbf0cde81e0a3d61b42040ee4b8d6fb7851a1ec
solorexd <arshad60971@gmail.com>
2025-10-18 01:37:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -114,7 +114,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
 # ------------------------------------
 # ------------------------------------
 # ------------------------------------
-STRING1 =  "BQFx9VQAm73DJmE-dNeLdkUYyAtQ1ns5DwJ5ahA9KgvPlmyKgK6hBodc2D4hGTCU7xs1KWg8mPrSy9jwc0SPSlwhjUAg25px5yn-xTMbC-rnMcSX29rQaJLLggftu7P7zY7H2GhcgkQjr15n-fnZqMeuIpgECfQQKDa4hpHsJlt4L0xPKJoSopDfOGipwg__FmUdgRUPtoOjcxB8C_VIK3cZ4Rvm8sJI873XNbnI6_K9imWLfXLH6ZNtLqqYhFuU0xx_5vaSJ-W0Rzuo9AVXJqz9JfPOGsskRVmZatIOhBFOK3_o_wRqjC_5e1L-_K7agp0D-4j9eLYRnCkjiAEZ4hYaHrMDcgAAAAG5gJzlAA"
+STRING1 =  "BQFx9VQAm637rn901ijiOWI2nic7bJCzBb2okg2s95gIJJ2LnyJtTwiD4P5E50WDfvksCiqbyugEPWwJ19s54RnUJfuhetkAyZcacPOPph7eq6dzbkoPgzVUwblawL0wVh_E2dT7QM7NBYr7s1jAQw9RNYslf_BpL40x0jQkeWIWAgBEN0UZjcQBoRXPPx9wBnfI1zk5AwimcwQCQ6B7eU9sn5soHlvBVrC8MhA8J248hWLfarCyo4CGuOsiyDd8mFfEZqAC6h4gSrFWuduGAffV2NHYH4INmn4H-aaQ0yafn5ic9VSXZxlVoXT6gnGlsnuohaIIxmfqDhxLhoHCp868QLKpOwAAAAG5gJzlAA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
